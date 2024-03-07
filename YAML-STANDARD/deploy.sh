8f45214387482de25160459f88e46b82d003344c
root <root@ip-172-31-8-117.eu-west-1.compute.internal>
2024-03-07 11:08:18 +0000

    Premier commit

--- YAML-STANDARD/deploy.sh
+++ YAML-STANDARD/deploy.sh
@@ -4,14 +4,13 @@ kubectl apply -f ./postgresql/secret-postgresql.yaml # secrets
 kubectl apply -f ./postgresql/configmap-postgresql.yaml  # Configs
 kubectl apply -f ./pgadmin/secret-pgadmin.yaml # secrets
 kubectl apply -f ./pgadmin/configmap-pgadmin.yaml # Configs
+kubectl apply -f ./fastapi/secret-fastapi.yaml #FastAPI secrets
+kubectl apply -f ./fastapi/configmap-fastapi.yaml #FastAPI Configs
 
 kubectl apply -f ./postgresql/persistentvolumeclaim.yaml
 kubectl apply -f ./postgresql/StatefulSet.yaml
-kubectl apply -f ./postgresql/deployment.yaml
+#kubectl apply -f ./postgresql/deployment.yaml
 kubectl apply -f ./postgresql/service.yaml
-
-kubectl apply -f ./fastapi/secret-fastapi.yaml #FastAPI secrets
-kubectl apply -f ./fastapi/configmap-fastapi.yaml #FastAPI Configs
 
 kubectl apply -f ./fastapi/deployment.yaml #FastAPI Deployment
 kubectl apply -f ./fastapi/service.yaml #FastAPI service
